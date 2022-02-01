5e87eb6bd3f726ef8b5eb189e06734b1411a5c24
Tizio889 <98579391+Tizio889@users.noreply.github.com>
2022-02-01 14:21:08 +0100

    Update Program.cs

--- DBConsoleApp/Program.cs
+++ DBConsoleApp/Program.cs
@@ -5,20 +5,31 @@ namespace DBConsoleApp
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            
+            
+
             try
             {
+
                 string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
                 
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
+                    
                     Console.WriteLine("\nQuery data example:");
                     Console.WriteLine("=========================================\n");
 
+                    string i;
+                    Console.WriteLine("inserire L'id da cercare");
+                    i = Console.ReadLine();
+
                     connection.Open();
 
-                    String sql = "SELECT CustomerID, ShipName FROM Orders WHERE CustomerID = 'PICCO' ";
+                    String sql = "SELECT CustomerID, ShipName FROM Orders WHERE CustomerID ='" + i + "'";
 
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
